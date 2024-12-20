5afe6bd4e1e4ad75993e753009e2d71b468582ab
Muhammad El-Mahdi <78433875+muhammadahmedelmahdy@users.noreply.github.com>
2024-12-20 15:44:09 +0200

    Add files via upload

--- env.py
+++ env.py
@@ -1,4 +1,7 @@
+import pickle
+
 import gymnasium
+import networkx as nx
 import numpy as np
 import pandas as pd
 from gymnasium import Env, spaces
@@ -17,6 +20,7 @@ class Environment(Env):
         self,
         max_couriers=4901,
         initial_timestamp=1666077600,
+        # 1665943248,
         time_step=300,
         episode_duration=3600,
     ):
@@ -38,7 +42,7 @@ class Environment(Env):
         self.action_mask = np.ones(max_couriers)
         self.action_space = Discrete(max_couriers)
         self.seed()
-
+        self.courier_stats = pd.read_csv("courier_stats.csv")
         self.done = False
         self.data_module = DataGenerationModule(
             "courier_wave_info_meituan.csv",
@@ -55,12 +59,49 @@ class Environment(Env):
         self.active_orders = self.data_module.get_orders_in_time_window(
             self.current_timestamp, self.episode_end_time
         )
+        self.map = self.read_graph()
+        # print(self.active_orders)
         # print(self.active_orders)
         self.active_couriers = self.data_module.get_active_couriers(
             self.current_timestamp
         )
+        # self.active_couriers.reset_index(inplace=True)
+
         # self.active_couriers.drop("order_ids", axis=1, inplace=True)
         self.reset()
+
+    def read_graph(self):
+        with open("undirected_graph.pkl", "rb") as f:
+            G = pickle.load(f)
+        return G
+
+    def find_time(self, start_node, end_node, G):
+        # print(start_node, end_node)
+        # Find the shortest path
+        try:
+
+            shortest_path = nx.shortest_path(G, source=start_node, target=end_node)
+            # print(f"The shortest path from {start_node} to {end_node} is: {shortest_path}")
+
+            # Get the values of the edges in the shortest path
+            edge_values = []
+            for i in range(len(shortest_path) - 1):
+                u, v = shortest_path[i], shortest_path[i + 1]
+                edge_data = G[u][v]  # Access edge attributes
+                edge_values.append(edge_data)  # Store the attributes
+            total_time = 0
+            # print("Edge values along the shortest path:")
+            for i, edge in enumerate(edge_values):
+                total_time += edge["weight"]
+                # print(f"Edge {shortest_path[i]} -> {shortest_path[i + 1]}: {edge['weight']}")
+
+        except nx.NetworkXNoPath:
+            total_time = 0
+            # print("No path found")
+        except nx.NodeNotFound as e:
+            # print(f"Error: {e}")
+            total_time = -2
+        return total_time
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
@@ -93,8 +134,10 @@ class Environment(Env):
         """
         Execute one step within the environment.
         """
-        #print(action)
-        #print(len(self.active_orders))
+        # print(action)
+        # print(len(self.active_couriers))
+        # print(len(self.active_orders))
+        action = int(action)
         if action not in range(self.active_couriers.shape[0]):
             self.reward -= 10
             return self.state, self.reward, self.done, False, {}
@@ -103,23 +146,59 @@ class Environment(Env):
             self.done = True
             return self.state, self.reward, self.done, False, {}
 
-        #print(self.reward)
+        # print(self.reward)
         order = self.active_orders.iloc[0]
         selected_courier = self.active_couriers.iloc[action]
+        # print(selected_courier)
+        # print(order)
         distance = np.sqrt(
             (selected_courier["grab_lat"] - order["sender_lat"]) ** 2
             + (selected_courier["grab_lng"] - order["sender_lng"]) ** 2
         )
-        self.reward += 10 / (distance + 1)
+        distance_reward = 10 / (distance + 1)
+
+        # if action in self.active_couriers.index:
+        #     print("Index 464 exists.")
+        # else:
+        #     print("Index 464 does not exist.")
         self.active_couriers.loc[action, "unfulfilled_orders"] += 1
+
+        avg_load = self.active_couriers["unfulfilled_orders"].mean()
+        courier_load = self.active_couriers.loc[action, "unfulfilled_orders"]
+        if courier_load - avg_load + 1 == 0:
+            load_balance_penalty = 0
+        else:
+            load_balance_penalty = 1 / (courier_load - avg_load + 1)
+
+        # Time reward
+        order_location = (order["sender_lng"], order["sender_lat"])
+        courier_location = (selected_courier["grab_lng"], selected_courier["grab_lat"])
+        # print(courier_location)
+        travel_time = self.find_time(courier_location, order_location, self.map)
+        travel_time_penalty = 1 / (travel_time + 1)
+        self.reward += distance_reward + load_balance_penalty + travel_time_penalty
+
         self.active_orders = self.active_orders.iloc[1:]
+        # self.reward +=
         self.update_action_mask()
         self.state = self.get_state()
+        # print(self.state)
+        # print(self.reward)
         if self.active_orders.shape[0] == 0:
             # print("No orders left")
             # print("we are done")
             self.done = True
-        return self.state, self.reward, self.done, False, {}
+        return (
+            self.state,
+            self.reward,
+            self.done,
+            False,
+            {
+                "distance": distance,
+                "load": (courier_load - avg_load),
+                "time": travel_time,
+            },
+        )
 
     def action_masks(self):
         return self.action_mask
@@ -132,9 +211,21 @@ class Environment(Env):
             # Mask couriers with valid data only
             if not (courier == [-1, -1, -1, -1, -1]).all():
                 self.action_mask[i] = 1
+        # # Get the first order in the queue
+        # first_order = self.active_orders.iloc[0]
+        # order_location = (first_order["sender_lng"], first_order["sender_lat"])
+        # for i, courier in self.active_couriers.iterrows():
+        #     courier_location = (courier["grab_lng"], courier["grab_lat"])
+        #     travel_time = self.find_time(courier_location, order_location, self.map)
+        #     if travel_time >= 0:  # Valid path found
+        #         self.action_mask[i] = 1
+        #     else:
+        #         self.action_mask[i] = 0
 
     def reset(self, seed=None):
         self.current_timestamp = self.current_timestamp + self.time_step
+        if self.current_timestamp == 1666627966:
+            self.current_timestamp = 1666077600
         self.episode_end_time = self.current_timestamp + self.episode_duration
         self.reward = 0
         # Retrieve orders for the entire time window
@@ -145,10 +236,12 @@ class Environment(Env):
             self.current_timestamp
         )
         self.active_couriers.drop("order_ids", axis=1, inplace=True)
+        self.active_couriers.reset_index(drop=True, inplace=True)
 
         self.done = False
         self.update_action_mask()
         self.state = self.get_state()
+        # print("time at the beginning of the episode: ", self.current_timestamp)
         return self.state, {}
 
     def get_state(self):
@@ -190,8 +283,8 @@ class Environment(Env):
         }
 
 
-# Create the environment
-env = Environment()
+# # Create the environment
+# env = Environment()
 
-# Check the environment
-check_env(env)
+# # Check the environment
+# check_env(env)
